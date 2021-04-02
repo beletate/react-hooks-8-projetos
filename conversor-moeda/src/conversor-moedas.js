d25cee9514c36b6212d98b2f60004711643b7408
beletate <vinicius_beletate@hotmail.com>
2021-04-02 15:07:28 -0300

    Finalizando formulario de conversao

--- conversor-moeda/src/conversor-moedas.js
+++ conversor-moeda/src/conversor-moedas.js
@@ -1,44 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './conversor-moedas.css';
 import {
-  Jumbotron,
-  Button,
-  Form,
-  Col,
-  Spinner
+  Jumbotron, Button, Form, Col, Spinner, Alert, Modal
 } from 'react-bootstrap'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import ListarMoedas from './listar-moedas'
 
 function ConversorMoedas() {
+
+  const [valor, setValor] = useState('1')
+  const [moedaDe, setMoedaDe] = useState('BRL')
+  const [moedaPara, setMoedaPara] = useState('USD')
+  const [exibirSpinner, setExibirSpinner] = useState(false)
+  const [formValidado, setFormValidado] = useState(false)
+  const [exibirModal, setExibirModal] = useState(false)
+  const [resultadoConversao, setResultadoConversao] = useState('')
+
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''))
+  }
+
+  function handleMoedaDe(event) {
+    setMoedaDe(event.target.value)
+  }
+
+  function handleMoedaPara(event) {
+    setMoedaPara(event.target.value)
+  }
+
+  function handleFecharModal(event){
+    setValor('1')
+    setMoedaDe('BRL')
+    setMoedaPara('USD')
+    setFormValidado(false)
+    setExibirModal(false)
+
+  }
+
+  function converter(event) {
+    event.preventDefault();
+    setFormValidado(true)
+    if(event.currentTarget.checkValidity() === true){
+      // TODO implementar a chamada ao Fixer.io
+
+    }
+
+  }
+
   return (
     <div>
       <h1>Conversor de Moedas</h1>
+      <Alert variant="danger" show={false}>
+        Erro ao obter dados de conversão, tente novamente.
+      </Alert>
       <Jumbotron>
-        <Form>
+        <Form onSubmit={converter} noValidate validated={formValidado}>
           <Form.Row>
             <Col sm="3">
-              <Form.Control 
-              placeholfer="" 
-              value={1}
-              required/>
+              <Form.Control
+                placeholder="0"
+                value={valor}
+                onChange={handleValor}
+                required />
             </Col>
             <Col sm="3">
-              <Form.Control as="select"></Form.Control>
+              <Form.Control as="select"
+                value={moedaDe}
+                onChange={handleMoedaDe}>
+                <ListarMoedas />
+              </Form.Control>
             </Col>
-            <Col sm="1" className="text-center" style={{paddingTop:'5px'}}>
-              <FontAwesomeIcon icon={faAngleDoubleRight}/>
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
             </Col>
             <Col sm="3">
-              <Form.Control as="select"></Form.Control>
+              <Form.Control as="select"
+                value={moedaPara}
+                onChange={handleMoedaPara}>
+                <ListarMoedas />
+              </Form.Control>
             </Col>
             <Col sm="2">
               <Button variant="success" type="submit">
-                <Spinner animation="border" size="sm"/>Converter</Button>
+                <span className={exibirSpinner ? null : 'hidden'}>
+                  <Spinner animation="border" size="sm" />
+                </span>
+                <span  className={exibirSpinner ? 'hidden' : null}>
+                  Converter
+                </span>
+              </Button>
             </Col>
           </Form.Row>
         </Form>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {resultadoConversao}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={handleFecharModal}>
+              Nova Conversão
+              </Button>
+          </Modal.Footer>
+        </Modal>
       </Jumbotron>
     </div>
   );

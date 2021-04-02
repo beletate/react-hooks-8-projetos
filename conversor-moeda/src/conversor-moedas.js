7af521b7f721ebbc2c9ff0c936a81526089a2b0b
beletate <vinicius_beletate@hotmail.com>
2021-04-02 15:34:32 -0300

    Implementando API para obter valores das moedas

--- conversor-moeda/src/conversor-moedas.js
+++ conversor-moeda/src/conversor-moedas.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
-import './conversor-moedas.css';
+import axios from 'axios'
 import {
   Jumbotron, Button, Form, Col, Spinner, Alert, Modal
 } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 import ListarMoedas from './listar-moedas'
+import './conversor-moedas.css';
+
 
 function ConversorMoedas() {
 
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3';
+
+  const [resultadoConversao, setResultadoConversao] = useState('')
   const [valor, setValor] = useState('1')
   const [moedaDe, setMoedaDe] = useState('BRL')
   const [moedaPara, setMoedaPara] = useState('USD')
   const [exibirSpinner, setExibirSpinner] = useState(false)
   const [formValidado, setFormValidado] = useState(false)
   const [exibirModal, setExibirModal] = useState(false)
-  const [resultadoConversao, setResultadoConversao] = useState('')
+  const [exibirMsgErro, setExibirMsgErro] = useState(false)
 
   function handleValor(event) {
     setValor(event.target.value.replace(/\D/g, ''))
@@ -29,7 +34,7 @@ function ConversorMoedas() {
     setMoedaPara(event.target.value)
   }
 
-  function handleFecharModal(event){
+  function handleFecharModal(event) {
     setValor('1')
     setMoedaDe('BRL')
     setMoedaPara('USD')
@@ -41,17 +46,46 @@ function ConversorMoedas() {
   function converter(event) {
     event.preventDefault();
     setFormValidado(true)
-    if(event.currentTarget.checkValidity() === true){
-      // TODO implementar a chamada ao Fixer.io
+    if (event.currentTarget.checkValidity() === true) {
+      setExibirSpinner(true)
+      axios
+        .get(FIXER_URL)
+        .then(res => {
+          const cotacao = obterCotacao(res.data)
+          if(cotacao){
+            setResultadoConversao(`${valor} ${moedaDe} = ${cotacao} ${moedaPara}`)
+            setExibirModal(true)
+            setExibirSpinner(false)
+            setExibirMsgErro(false)
+          }else{
+            exibirErro()
+          }
+        })
+        .catch(err => exibirErro())
 
     }
 
   }
 
+  function obterCotacao(dadosCotacao) {
+    if (!dadosCotacao || dadosCotacao.success !== true) {
+      return false
+    }
+    const cotacaoDe = dadosCotacao.rates[moedaDe]
+    const cotacaoPara = dadosCotacao.rates[moedaPara]
+    const cotacao = (1 / cotacaoDe * cotacaoPara) * valor
+    return cotacao.toFixed(2)
+  }
+
+  function exibirErro(){
+    setExibirMsgErro(true)
+    setExibirSpinner(false)
+  }
+
   return (
     <div>
       <h1>Conversor de Moedas</h1>
-      <Alert variant="danger" show={false}>
+      <Alert variant="danger" show={exibirMsgErro}>
         Erro ao obter dados de conversão, tente novamente.
       </Alert>
       <Jumbotron>
@@ -86,7 +120,7 @@ function ConversorMoedas() {
                 <span className={exibirSpinner ? null : 'hidden'}>
                   <Spinner animation="border" size="sm" />
                 </span>
-                <span  className={exibirSpinner ? 'hidden' : null}>
+                <span className={exibirSpinner ? 'hidden' : null}>
                   Converter
                 </span>
               </Button>

246dec20781ac856460239508c40c98001848862
beletate <vinicius_beletate@hotmail.com>
2021-04-27 17:02:29 -0300

    Replicando aplicacao gerenciador tarefa

---
+++ gerenciador-de-tarefas-api-cliente/README.md
@@ -0,0 +1,14 @@
+# Gerenciador de Tarefas
+
+![Gerenciador](img/gerenciamento.gif)
+
+## Scripts disponíveis
+
+### `npm start`
+
+Inicializa a aplicação.\
+Abra [http://localhost:3000](http://localhost:3000) para ver no navegador.
+
+### `npm test`
+
+Executa testes em relação a conversão das moedas.
---
+++ gerenciador-de-tarefas-api-cliente/src/gerenciador-tarefas.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import GerenciadorTarefas from './gerenciador-tarefas';
+import ReactDOM from 'react-dom'
+
+describe('Teste do componente gerenciador de tarefas', () => {
+  it('deve renderizar o componente sem erro', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<GerenciadorTarefas />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})

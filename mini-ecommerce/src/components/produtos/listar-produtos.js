bfe2449d2c7a09cdf105e09060c348db938bcaf8
beletate <vinicius_beletate@hotmail.com>
2021-05-06 20:36:29 -0300

    Criando os componentes da aplicacao

---
+++ mini-ecommerce/src/components/produtos/listar-produtos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import placeholder from '../../imagens/286x180.png'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+function ListarProdutos() {
+    return (
+        <h1>Produtos</h1>
+    )
+}
+
+export default ListarProdutos;

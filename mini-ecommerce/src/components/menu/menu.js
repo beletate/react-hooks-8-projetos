f56005056a061f8f88c75e8cd391de1d765a3952
beletate <vinicius_beletate@hotmail.com>
2021-05-14 19:48:57 -0300

    Criacao da navbar para gerenciar o carrinho

--- mini-ecommerce/src/components/menu/menu.js
+++ mini-ecommerce/src/components/menu/menu.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faShoppingBasket,
+    faCashRegister,
+    faShoppingCart
+} from '@fortawesome/free-solid-svg-icons'
 
 function Menu() {
     return (
-        <h1>Menu</h1>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="">Mini Ecommerce</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    <NavDropdown
+                        title={
+                            <div style={{ display: 'inline-block' }}>
+                                <FontAwesomeIcon icon={faShoppingCart} />
+                                &nbsp;
+                                Carrinho
+                            </div>
+                        }
+                        drop="left">
+                        <NavDropdown.Item href="">
+                            <FontAwesomeIcon icon={faShoppingBasket} />
+                                &nbsp;
+                                <strong>Produtos</strong>
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        {/* ITENS DO CARRINHO */}
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="" data-testid="total-carrinho">
+                            Total: R$ {/* chamar função de cálculo de total */}
+                        </NavDropdown.Item>
+                        <span>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item
+                                href=""
+                                style={{ color: 'green' }}>
+                                    <FontAwesomeIcon icon={faCashRegister} />
+                                    &nbsp;
+                                    Finalizar compra
+                            </NavDropdown.Item>
+                        </span>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 

5fff337fa488c24dd2ca308d8f10d9162011e6b7
beletate <vinicius_beletate@hotmail.com>
2021-05-20 20:43:52 -0300

    Finalizando formulario de checkout

--- mini-ecommerce/src/components/checkout/checkout.js
+++ mini-ecommerce/src/components/checkout/checkout.js
@@ -98,6 +98,98 @@ function Checkout(props) {
                             </Form.Control.Feedback>
                     </Col>
                 </Form.Group>
+
+                <Form.Group as={Row} controlId="estado">
+                    <Form.Label column sm={3}>
+                        Estado
+                    </Form.Label>
+                    <Col sm={9}>
+                        <Form.Control
+                            as="select"
+                            name="estado"
+                            data-testid="estado">
+
+                        </Form.Control>
+                        <Form.Control.Feedback type="invalid">
+                            Selecione o seu estado.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="cidade">
+                    <Form.Label column sm={3}>
+                        Cidade
+                    </Form.Label>
+                    <Col sm={9}>
+                        <Form.Control
+                            as="select"
+                            name="cidade"
+                            data-testid="cidade">
+                            <option value="">Selecione a cidade</option>
+                        </Form.Control>
+                        <Form.Control.Feedback type="invalid">
+                            Selecione a sua cidade.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="cep">
+                    <Form.Label column sm={3}>
+                        CEP
+                    </Form.Label>
+                    <Col sm={9}>
+                        <Form.Control
+                            type="text"
+                            placeholder="Digite o seu CEP"
+                            name="cep"
+                            data-testid="txt-cep" />
+                        <Form.Control.Feedback type="invalid">
+                            Digite o seu CEP.
+                        </Form.Control.Feedback>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="emailPromocional">
+                    <Form.Label column sm={12}>
+                        Deseja receber email com promoções?
+                    </Form.Label>
+                    <Form.Check
+                        inline
+                        name="emailPromocional"
+                        type="radio"
+                        id="promocaoSim"
+                        value="S"
+                        label="Sim"
+                        style={{ marginLeft: '15px' }} />
+
+                    <Form.Check
+                        inline
+                        name="emailPromocional"
+                        type="radio"
+                        id="promocaoNao"
+                        value="N"
+                        label="Não"
+                        style={{ marginLeft: '15px' }} />
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="termosCondicoes">
+                    <Form.Check
+                        name="termosCondicoes"
+                        label="Concordo com os termos e condições"
+                        style={{ marginLeft: '15px' }}
+                        data-testid="check-termos-condicoes" />
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="finalizarCompra">
+                    <Col className="text-center" sm={12}>
+                        <Button
+                            type="submit"
+                            variant="success"
+                            data-testid="btn-finalizar-compra">
+                                Finalizar compra
+                            </Button>
+                    </Col>
+                </Form.Group>
             </Form>
 
 
